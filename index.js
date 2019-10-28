4015360f918e99c36dd6651e6d2cbecf420e9484
chrismjohnston <cmjohnston@gmail.com>
2019-10-28 15:09:57 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("\n It's alive \n")
+// import express from 'express; ES Modules'
+const express = require('express'); //CommonJS modules, and it is equivalent to the line above.
+
+const server = express();  // creates a server
+
+// request/route handlers
+
+// handles GET requests to / on loclhost:8000
+server.get('/', (req, res) => {
+    res.send('Hello Node 23!')
+});
+
+// listen for request in a particular port on localhost
+const port = 8000; // localhost:8000
+server.listen(port, () => console.log('\n=== API on port 8000 ===\n'));
+
+
+//const express = require('express');
+
+// const users = require('./data/hubs-model')
+
+// const server= express();
+
+// server.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+// server.get('/hobbits', (req, res) => {
+//   const hobbits = [
+//     {
+//       id: 1,
+//       name: 'Samwise Gamgee',
+//     },
+//     {
+//       id: 2,
+//       name: 'Frodo Baggins',
+//     },
+//   ]
+//   res.status(200).json(hobbits)
+// });
+
+// server.get('/users', (req, res) => {
+//   users.find()
+//   .then((users) => {
+//     res.status(200).json(users)
+//   })
+//   .catch((error) => {
+//     res.status(500).json({message: "unable to get users"})
+//   })
+// });
+
+// server.listen(8000, () => {
+//   console.log('API running on port 8000')
+// });
--- notes.md
+++ notes.md
@@ -1,11 +1,19 @@
-#Node API 1 notes
+# Node API 1 Notes
 
 - created repository on GH
 - cloned it
 - opened it in editor
-- add .gitignore ==> "npx gitignore node"
-- add 'package.json' ==> 'npm init -y'
-- added "server script inside package.json
-- install 'nodemon' as a developement dependency => 'npm i -D nodemon'
-- add an 'index.js' file to root folder with console.log
-- run the API using 'npm run server'
+- add `.gitignore` ==> `npx gitignore node`
+- add `package.json` ==> `npm init -y`
+- added "server" script inside `package.json`
+- install `nodemon` as a development dependency => `npm i -D nodemon`
+- add an `index.js` file to root folder with a console.log
+- run the API using `npm run server`
+
+## Basic Express Server
+
+- install `express` as a production dependency => `npm i express`
+
+_Typos, the bane of developers_
+
+'missing script: server'
